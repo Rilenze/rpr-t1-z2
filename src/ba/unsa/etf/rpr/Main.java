6afe872d399e9957da88a5e5e1cc4b0fc06315e1
Rijad <rburovic1@etf.unsa.ba>
2020-10-30 17:44:07 +0100

    2. commit

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -3,7 +3,7 @@ package ba.unsa.etf.rpr;
 import java.util.Scanner;
 
 public class Main {
-    public int sumaCifara(int broj) {
+    public static int sumaCifara(int broj) {
         int suma = 0;
         while (broj != 0) {
             suma += broj % 10;
@@ -13,5 +13,14 @@ public class Main {
     }
     public static void main(String[] args) {
         int n;
+        System.out.println("Unesite n: ");
+        Scanner ulaz = new Scanner(System.in);
+        n = ulaz.nextInt();
+        int i;
+        for (i=1; i<n; i++) {
+            if (i % sumaCifara(i) == 0) {
+                System.out.println(i);
+            }
+        }
     }
 }
